85f76e7bada91c1905c1e8d705f42a74cc3b141a
faazill <muhammedfazilk1897@gmail.com>
2024-12-25 12:15:44 +0530

    Initial commit for Visro project

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// JavaScript for Hello World Button
+document.getElementById('helloButton').addEventListener('click', function() {
+    document.getElementById('output').textContent = "Hello, Visro World!";
+    alert("Welcome to Visro!");
+});
